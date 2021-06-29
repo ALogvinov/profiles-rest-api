d4f85537f8bb15a7076f71916d77323ac1cfdf06
Alexander Logvinov <logvinovaleksander@gmail.com>
2021-06-29 11:13:31 +0300

    GIT ADD VIEWSET

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serualizes
 
@@ -36,3 +37,40 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Тестируем удаление объекта"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API Viewset"""
+    serializer_class = serualizes.HelloSerializer
+
+    def list(self, request):
+        """Return Hello message"""
+        an_apiview = ["Uses HTTP methods as function", "Is similar to a tradition Django view"]
+        return Response({'message': 'ok', 'an_apiview': an_apiview})
+
+    def create(self, request):
+        """Create new Hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        """Тест получения метода по id"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """Тест получения метода update по id"""
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """Тест получения метода partial_update по id"""
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """Тест получения метода partial_update по id"""
+        return Response({'http_method': 'DELETE'})
